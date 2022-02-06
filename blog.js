ba49aa9641a81346369ed813adad09c0c2522d6a
Adi1432 <69716922+Adi1432@users.noreply.github.com>
2022-02-06 16:56:23 +0530

    Add files via upload

---
+++ blog.js
@@ -0,0 +1,65 @@
+// let addToDoButton = document.getElementById("addToDo");
+// let toDoContainer = document.getElementById("toDoContainer");
+// let inputField = document.getElementById("inputField");
+
+// addToDoButton.addEventListener("click", function () {
+//   if (inputField.value != "") {
+//     var paragraph = document.createElement("p");
+//     paragraph.classList.add("paragraph-styling");
+//     paragraph.innerText = inputField.value;
+//     toDoContainer.appendChild(paragraph);
+//     inputField.value = "";
+
+//     paragraph.addEventListener("dblclick", function () {
+//       toDoContainer.removeChild(paragraph);
+//     });
+//   }
+// });
+let header = document.getElementById("header");
+let description = document.getElementById("descripton");
+let AddBlog = document.getElementById("addBlog");
+let con = document.getElementById("container");
+AddBlog.addEventListener("click", () => {
+  if (header.value != "" && description.value != "") {
+    let div1 = document.createElement("div");
+    div1.className = "blogContainer";
+    let h1 = document.createElement("h1");
+    h1.className = "headers";
+    h1.innerText = header.value;
+    let p = document.createElement("p");
+    p.className = "para";
+    p.innerText = description.value;
+    let div2 = document.createElement("div");
+    div2.className = "buttonsConatiners";
+    let deleteBtn = document.createElement("button");
+    deleteBtn.innerText = "delete";
+    let EditBtn = document.createElement("button");
+    EditBtn.innerText = "Edit";
+    div2.append(deleteBtn, EditBtn);
+    div1.append(h1, p, div2);
+    con.appendChild(div1);
+    header.value = "";
+    description.value = "";
+
+    deleteBtn.addEventListener("click", () => {
+      con.removeChild(div1);
+    });
+    EditBtn.addEventListener("click", () => {
+      header.value = h1.innerText;
+      description.value = h1.innerText;
+      let updateBtn = document.createElement("button");
+      updateBtn.innerText = "update";
+      div2.appendChild(updateBtn);
+      AddBlog.disabled = true;
+
+      updateBtn.addEventListener("click", () => {
+        h1.innerText = header.value;
+        p.innerText = description.value;
+        header.value = "";
+        description.value = "";
+        div2.removeChild(updateBtn);
+        AddBlog.disabled = false;
+      });
+    });
+  }
+});
